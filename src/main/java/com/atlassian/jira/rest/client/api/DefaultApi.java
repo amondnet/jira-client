cc3f98831535955f7b51d72bedcaa2a6c969b7c6
amond <amond@amond.net>
2021-04-16 00:17:37 +0900

    first

---
+++ src/main/java/com/atlassian/jira/rest/client/api/DefaultApi.java
@@ -0,0 +1,87 @@
+package com.atlassian.jira.rest.client.api;
+
+import com.atlassian.jira.rest.client.ApiClient;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2021-04-16T00:10:50.175246+09:00[Asia/Seoul]")
+public class DefaultApi {
+    private ApiClient apiClient;
+
+    public DefaultApi() {
+        this(new ApiClient());
+    }
+
+    @Autowired
+    public DefaultApi(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public ApiClient getApiClient() {
+        return apiClient;
+    }
+
+    public void setApiClient(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    /**
+     * Get features for project
+     * Returns the list of features for the given project. The project must be a Company-managed project.
+     * <p><b>200</b> - Returned if the request is successful.
+     * <p><b>400</b> - Returned if the given project is not a company-managed project or is not a jira software project.
+     * <p><b>401</b> - Returned if the authentication credentials are incorrect or missing.
+     * <p><b>403</b> - Returned if the user does not have the required permissions.
+     * <p><b>404</b> - Returned if there is no project corresponding to the given ID or key.
+     * @param projectIdOrKey The ID or the key of the project
+     * @return Object
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public Mono<Object> getFeaturesForProject(String projectIdOrKey) throws WebClientResponseException {
+        Object postBody = null;
+        // verify the required parameter 'projectIdOrKey' is set
+        if (projectIdOrKey == null) {
+            throw new WebClientResponseException("Missing the required parameter 'projectIdOrKey' when calling getFeaturesForProject", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), null, null, null);
+        }
+        // create path and map variables
+        final Map<String, Object> pathParams = new HashMap<String, Object>();
+
+        pathParams.put("projectIdOrKey", projectIdOrKey);
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders headerParams = new HttpHeaders();
+        final MultiValueMap<String, String> cookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] localVarAccepts = { 
+            "application/json"
+        };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] { "OAuth2", "basicAuth" };
+
+        ParameterizedTypeReference<Object> localVarReturnType = new ParameterizedTypeReference<Object>() {};
+        return apiClient.invokeAPI("/rest/api/3/project/{projectIdOrKey}/features", HttpMethod.GET, pathParams, queryParams, postBody, headerParams, cookieParams, formParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    }
+}
